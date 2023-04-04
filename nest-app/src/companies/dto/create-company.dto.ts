4e674e26cae82b1a76fabe0d5d963943a8b54641
Gabriel Lira <gabrieldeio40@gmail.com>
2023-04-04 12:06:05 -0300

    Refactor(CompanyService) : Improve exception handling

--- nest-app/src/companies/dto/create-company.dto.ts
+++ nest-app/src/companies/dto/create-company.dto.ts
@@ -1,7 +1,13 @@
+import { IsNotEmpty, IsString } from "class-validator";
+
 export class CreateCompanyDto{
+    @IsNotEmpty()
     cnpj : string;
+    @IsString()
     adress : string;
+    @IsString()
     name : string;
+    @IsString()
     phoneNumber : string;
 }
 
